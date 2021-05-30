6452770677b0ebcf96c9a21046596399860e04d8
Nayanigaonkar <nayanigaonkar41@gmail.com>
2021-05-30 20:12:11 +0530

    Changes

--- textAnalyserJs.js
+++ textAnalyserJs.js
@@ -1,4 +1,5 @@
-
-$(".textAnalyzeBtn").click(function analyzeTest () {
-    console.log("In function text analyzer");
-}) 
+$(document).ready(function() {
+    $(".textAnalyzeBtn").click(function analyzeTest () {
+        console.log("In function text analyzer");
+    }) 
+});
