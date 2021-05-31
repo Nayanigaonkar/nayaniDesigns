cbcc5b32a47a9cb143f872d8398e169e4fb7172e
Nayanigaonkar <nayanigaonkar41@gmail.com>
2021-05-31 23:50:27 +0530

    changes

--- textAnalyserJs.js
+++ textAnalyserJs.js
@@ -1,5 +1,17 @@
-$(document).ready(function() {
-    $(".textAnalyzeBtn").click(function analyzeTest () {
+$(document).ready(function () {
+    $(".textAnalyzeBtn").click(function analyzeTest() {
         console.log("In function text analyzer");
-    }) 
+
+        var textValue = $(".textArea").val();
+
+        var textCount = $(".textArea").val().length;
+
+        // $(".textOutpputDiv").value = getComputedStyle(".textArea").getPropertyValue("font-family");
+
+        $(".textOutputDiv").html("<b> Your Input </b> :" + textValue +
+            " </br> <b> Input Length </b>: " + textCount +
+            "</br> <b> Input Type </b>: " + typeof textValue);
+
+    });
+
 });
